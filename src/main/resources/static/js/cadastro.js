fce19a9b46e491869b0dc403fbfea25f9fbc89dc
Felipe Carvalho de Souza <desouzafelipecarvalho@gmail.com>
2024-09-26 21:57:38 -0300

    Adicionando máscara de CPF

--- src/main/resources/static/js/cadastro.js
+++ src/main/resources/static/js/cadastro.js
@@ -11,6 +11,39 @@ function updateFormAction() {
     }
 }
 
+function aplicarMascaraCPF(cpf) {
+	cpf = cpf.replace(/\D/g, '');
+
+	cpf = cpf.replace(/(\d{3})(\d)/, '$1.$2'); 
+	cpf = cpf.replace(/(\d{3})(\d)/, '$1.$2'); 
+	cpf = cpf.replace(/(\d{3})(\d{1,2})$/, '$1-$2');
+	return cpf;
+}
+
+function formatarCPF() {
+	const cpfInput = document.getElementById('cpf');
+	cpfInput.value = aplicarMascaraCPF(cpfInput.value);
+}
+
+function validarCPF(cpf) {
+	const regex = /^\d{3}\.\d{3}\.\d{3}-\d{2}$/;
+	return regex.test(cpf);
+}
+
+function validarFormulario(event) {
+	updateFormAction();
+
+	const cpfInput = document.getElementById('cpf');
+	const cpf = cpfInput.value;
+
+	if (!validarCPF(cpf)) {
+		alert('Por favor, insira um CPF válido');
+		event.preventDefault();
+		return false;
+	}
+	return true;
+}
+
 window.onload = function() {
     updateFormAction();
 };
